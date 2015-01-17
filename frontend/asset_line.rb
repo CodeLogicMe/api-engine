0da63d713b2c7eb575a2ac18e35927e55edb655d
lukelex <lukas@codelogic.me>
2015-01-17 17:43:09 +0100

    Refactoring AssetLine to improve debug

--- frontend/asset_line.rb
+++ frontend/asset_line.rb
@@ -3,7 +3,11 @@ class AssetLine
     env = options.fetch(:env) { ENV['RACK_ENV'] }
     assets_root = options.fetch(:root)
 
-    @compilers = [CSS.new(env, assets_root)]
+    @compilers = [
+      CSS.new(env, assets_root),
+      Stylus.new(env, assets_root),
+      NullCompiler.new
+    ]
   end
 
   def fetch(filename)
@@ -12,31 +16,46 @@ class AssetLine
       .compile(filename)
   end
 
+  UnhandableAsset = Class.new(StandardError)
+
   private
 
-  def js?(filename)
-    filename.match /\.(js|coffee)$/i
-  end
-
   class CSS
-    require 'stylus'
-
     def initialize(env, root)
       @env = env
       @root = File.join root, 'stylesheets'
     end
 
     def can_handle?(filename)
-      filename.match /\.(css|styl)$/i
+      filename.match(/\.css$/i) &&
+        File.exist?(file_path(filename))
     end
 
     def compile(filename)
-      path = File.join(@root, "#{extensionless(filename)}.styl")
-      Stylus.compile File.new(path), **css_options
+      File.read(file_path(filename))
     end
+
+    private
 
     def extensionless(filename)
       filename.gsub /\.css$/, ''
+    end
+
+    def file_path(filename)
+      File.join(@root, "#{extensionless(filename)}.css")
+    end
+  end
+
+  class Stylus < CSS
+    require 'stylus'
+
+    def compile(filename)
+      path = File.join(@root, "#{extensionless(filename)}.styl")
+      ::Stylus.compile File.new(path), **css_options
+    end
+
+    def file_path(filename)
+      File.join(@root, "#{extensionless(filename)}.styl")
     end
 
     def css_options
@@ -48,6 +67,12 @@ class AssetLine
           compress: false
         }
       }[@env]
+    end
+  end
+
+  class NullCompiler
+    def can_handle?(_)
+      fail UnhandableAsset
     end
   end
 end
