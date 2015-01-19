2254676472c0c4739964af9d41e56cbf34bb9583
lukelex <lukas@codelogic.me>
2015-01-19 23:19:31 +0100

    Enabling Mongoid query logs in development

--- config/initializers/mongoid.rb
+++ config/initializers/mongoid.rb
@@ -1,3 +1,10 @@
 require 'mongoid'
 
 Mongoid.load! 'config/mongoid.yml', ENV['RACK_ENV']
+
+if ENV['RACK_ENV'] == 'development'
+  Mongoid.logger.level = Logger::DEBUG
+  Moped.logger.level = Logger::DEBUG
+  Mongoid.logger = Logger.new($stdout)
+  Moped.logger = Logger.new($stdout)
+end
