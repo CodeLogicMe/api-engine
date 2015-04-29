7f9c1cdafa1fde17f0d5254b5a5bd1fb740022b9
Lukas Alexandre <lukas@codelogic.me>
2015-04-29 20:59:36 +0200

    Enabling the client app to query for entities separetly

--- frontend/server.rb
+++ frontend/server.rb
@@ -18,26 +18,28 @@ class Frontend < ::Grape::API
   resource 'apps' do
     get do
       {
-        apps: Models::Client.first.apps.map(&app_fields)
+        apps: Models::Client.first.apps.map { |app| app_attrs(app) }
       }
     end
 
     get '/:app_id' do
       app = Models::Client.first.apps.find_by(system_name: params.app_id)
       {
-        app: {
-          id: app.system_name,
-          name: app.name,
-          entities: app.app_config.entities.map { |entity| entity[:name] },
-        },
-        entities: app.app_config.entities.map { |entity|
-          {
-            id: entity[:name],
-            name: entity[:name],
-            fields: entity[:fields].map { |field| field_id(app, entity, field) }
-          }
-        },
-        fields: app.app_config.entities.flat_map { |entity| fields(app, entity) }
+        app: app_attrs(app),
+        entities: app.app_config.entities.map { |entity| entity_attrs(app, entity) },
+        fields: app.app_config.entities.flat_map { |entity| fields_attrs(app, entity) }
+      }
+    end
+  end
+
+  resource :entities do
+    get ':id' do
+      ids = params.id.split('#')
+      app = Models::Client.first.apps.find_by(system_name: ids[0])
+      entity = app.app_config.entities.find { |entity| entity['name'] == ids[1] }
+      {
+        entity: entity_attrs(app, entity),
+        fields: fields_attrs(app, entity)
       }
     end
   end
@@ -63,7 +65,7 @@ class Frontend < ::Grape::API
   end
 end
 
-def fields(app, entity)
+def fields_attrs(app, entity)
   entity[:fields].map do |field|
     {
       id: field_id(app, entity, field),
@@ -80,8 +82,20 @@ def field_id(app, entity, field)
   "#{app.system_name}##{entity[:name]}##{field[:name]}"
 end
 
-def app_fields
-  lambda { |app|
-    { id: app.system_name, name: app.name }
+def app_attrs(app)
+  {
+    id: app.system_name,
+    name: app.name,
+    public_key: app.public_key,
+    private_key: app.private_key.secret,
+    entities: app.app_config.entities.map { |entity| "#{app.system_name}##{entity[:name]}" }
+  }
+end
+
+def entity_attrs(app, entity)
+  {
+    id: "#{app.system_name}##{entity[:name]}",
+    name: entity[:name],
+    fields: entity[:fields].map { |field| field_id(app, entity, field) }
   }
 end
