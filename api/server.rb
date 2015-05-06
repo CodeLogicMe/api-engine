2b790ebd21664dcf7f75e441e482ba39205e7adb
Lukas Alexandre <lukas@codelogic.me>
2015-05-07 01:45:33 +0200

    Fixing log path

--- api/server.rb
+++ api/server.rb
@@ -8,10 +8,16 @@ require_relative './frontend'
 
 class API < Grape::API
   if ENV['RACK_ENV'] == 'production'
-    logger Logger.new GrapeLogging::MultiIO.new(STDOUT, File.open(File.join(__dir__, "../../shared/log/production.log"), 'a'))
+    logger Logger.new GrapeLogging::MultiIO.new(STDOUT, File.open(File.join(__dir__, "../../../shared/log/production.log"), 'a'))
   else
     logger Logger.new GrapeLogging::MultiIO.new(STDOUT, File.open(File.join(__dir__, "../log/#{ENV['RACK_ENV']}.log"), 'a'))
   end
+
+  require 'logger'
+  Logger.class_eval { alias :write :'<<' }
+  logger = ::Logger.new(::File.new("log/app.log","a+"))
+
+  use Rack::CommonLogger, logger
 
   rescue_from :all do |e|
     MyAPI.logger.error e
