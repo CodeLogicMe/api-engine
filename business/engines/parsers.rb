490361c8f8224048e4641e70adda8b719cccf833
Lukas Alexandre <lukas@codelogic.me>
2015-05-10 13:48:19 +0200

    Making sure that we're using the global Datetime while parsing dates

--- business/engines/parsers.rb
+++ business/engines/parsers.rb
@@ -3,5 +3,5 @@ require 'bigdecimal'
 module Parsers
   Text = -> (value) { value.to_s }
   Number = -> (value) { value.nil? ? nil : BigDecimal.new(value) }
-  Datetime = -> (value) { DateTime.parse(value.to_s) }
+  Datetime = -> (value) { ::DateTime.parse(value.to_s) }
 end
