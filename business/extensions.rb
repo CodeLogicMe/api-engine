eec0b8f2e7ee0ec794d11fcb0e506b05b05f9536
Lukas Alexandre <lukas@codelogic.me>
2015-07-13 21:58:40 +0200

    Extracting Passwordable into a separate file

--- business/extensions.rb
+++ business/extensions.rb
@@ -1,32 +1,5 @@
 module Extensions
-  module Passwordable
-    require 'bcrypt'
-
-    def self.included(recipient)
-      recipient.class_eval do
-        field :password_hash, type: String
-        validates_presence_of :password_hash
-      end
-    end
-
-    def password
-      ::BCrypt::Password.new password_hash
-    end
-
-    def password=(new_password)
-      self.password_hash = to_crypt_hash(new_password)
-    end
-
-    def password_checks?(pass)
-      password == pass
-    end
-
-    private
-
-    def to_crypt_hash(pass)
-      ::BCrypt::Password.create pass
-    end
-  end
+  require_relative './extensions/passwordable'
 
   module Sluggable
     def slug(field, on: nil)
