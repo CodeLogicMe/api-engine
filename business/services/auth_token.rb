51121e2e78627e7dc4b927807c55156a4a0a9d68
Lukas Alexandre <lukas@codelogic.me>
2015-07-14 17:04:50 +0200

    Fixing missing dependency for AuthToken

--- business/services/auth_token.rb
+++ business/services/auth_token.rb
@@ -1,6 +1,7 @@
 require 'redis'
 require 'redis-namespace'
 require 'securerandom'
+require_relative '../../config/initializers/redis'
 
 module Services::AuthToken
   STORE = Redis::Namespace.new(:tokens, redis: ::REDIS_CLIENT)
