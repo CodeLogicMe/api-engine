a551a1f0f9040caeaedea62f938a889afaae0f4d
Lukas Alexandre <lukas@codelogic.me>
2015-08-04 22:58:33 +0200

    Fixing statistics

--- api/frontend/statistics.rb
+++ api/frontend/statistics.rb
@@ -5,11 +5,9 @@ module Frontend
   class Statistics < Grape::API
     helpers AuthHelpers
 
-    before { authenticate! }
-
     get '/statistics/:api_id' do
       authenticate!
-      api = current_client.apis.find_by(system_name: params.api_id)
+      api = current_client.apis.find(params.api_id)
       { statistic: Serializers::Stats.new(api).to_h }
     end
   end
