4bbcf4b287e8d4ba63fb51e4a425d37e7b2ca420
Lukas Alexandre <lukas@codelogic.me>
2015-08-10 00:54:15 +0200

    Fixing PG's production connection

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -2,5 +2,9 @@ require 'grape/activerecord'
 require 'active_record'
 require 'pg'
 
-Grape::ActiveRecord.database_file = 'config/db/sql.yml'
+if ENV['RACK_ENV'] == 'production'
+  Grape::ActiveRecord.database_url = ENV['POSTGRESQL_URL_INT']
+else
+  Grape::ActiveRecord.database_file = 'config/db/sql.yml'
+end
 ActiveRecord::Migrator.migrations_paths += ['config/db/migrations']
