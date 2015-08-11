5fdb7a740b0b3aa6d76619fedcdcde02d787d40a
Lukas Alexandre <lukas@codelogic.me>
2015-08-11 12:14:42 +0200

    Reducing AR log levels in production

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -3,6 +3,7 @@ require 'active_record'
 require 'pg'
 
 if ENV['RACK_ENV'] == 'production'
+  ActiveRecord::Base.logger.level = 1
   Grape::ActiveRecord.database_url = ENV['POSTGRESQL_URL_INT']
 else
   Grape::ActiveRecord.database_file = 'config/db/sql.yml'
